48e0f1013c91014200f691e9157eacdda1f0add0
Joan000111 <aracely.a.cifuentes.v@gmail.com>
2023-10-05 16:53:26 -0300

    añadido botn de ingreso sn validar y regreso al login

--- app/src/main/java/com/example/proyectoprototipo/MainActivity.java
+++ app/src/main/java/com/example/proyectoprototipo/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.proyectoprototipo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button ingresarr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ingresarr = findViewById(R.id.ingresar);
+        ingresarr.setOnClickListener(new View.OnClickListener() {
+            //aqui tambien habria que validar el tema del login
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        MainActivity.class
+                );
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/proyectoprototipo/busqueda.java
+++ app/src/main/java/com/example/proyectoprototipo/busqueda.java
@@ -2,13 +2,27 @@ package com.example.proyectoprototipo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class busqueda extends AppCompatActivity {
+
+    ImageButton volverlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda);
+
+        volverlogin = findViewById(R.id.volver2);
+        volverlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
